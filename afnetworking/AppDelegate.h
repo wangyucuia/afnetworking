b68446f127d72ff19c0fc9d7720c473f3285bd5f
王玉翠 <m15201386258@163.com>
2016-08-03 13:01:27 +0800

    Initial Commit

---
+++ afnetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  afnetworking
+//
+//  Created by 王玉翠 on 16/8/3.
+//  Copyright © 2016年 王玉翠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ afnetworking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  afnetworking
+//
+//  Created by 王玉翠 on 16/8/3.
+//  Copyright © 2016年 王玉翠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
